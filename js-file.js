63c087d05243c6b04ce4e9ab08feca2cf13a2918
William Ekelund <bwekelund@gmail.com>
2022-06-08 11:09:19 +0200

    Calculator now only shows 3 decimal fractions. Added a footer with github link.

--- js-file.js
+++ js-file.js
@@ -30,15 +30,24 @@ operatorKeys.forEach((key) =>
   key.addEventListener("click", (e) => {
     removeClasses();
     if (operator != null) {
-      two = parseFloat(currentCalc.textContent);
+      operands[currentOperand] = parseFloat(currentCalc.textContent);
       operate();
-    }
+      currentOperand++;
+      updatePreviousCalc();
       operator = e.target.id;
-      one = parseFloat(currentCalc.textContent);
+      operands[currentOperand - 1] = parseFloat(currentCalc.textContent);
+      key.classList.add("operatorSelected");
+      isWaiting = true;
+      onlyOperator = false;
+    } else{
+      operator = e.target.id;
+      operands[currentOperand - 1] = parseFloat(currentCalc.textContent);
       key.classList.add("operatorSelected");
       isWaiting = true;
       updatePreviousCalc();
       onlyOperator = false;
+
+    }
   }));
 
 function removeClasses() {
@@ -54,13 +63,13 @@ equalsKey.addEventListener("click", equals);
 
 function equals() {
   if (operator != null) {
-    two = parseFloat(currentCalc.textContent);
+    operands[currentOperand] = parseFloat(currentCalc.textContent);
     operate();
     operator = null;
     removeClasses();
     isWaiting = true;
     updatePreviousCalc();
-    clearOperands()
+    operands[currentOperand++]
     onlyOperator = true;
   }
 }
@@ -84,15 +93,14 @@ function clearCurrentCalc() {
 }
 
 function isOperandsNull() {
-  if(one == null && two == null){
+  if(operands[currentOperand - 1]  == null && operands[currentOperand] == null){
     return true;
   }
   return false;
 }
 
 function clearOperands(){
-  one = null;
-  two = null;
+  operands = [];
 }
 
 function isOperatorNull(){
@@ -107,8 +115,7 @@ function clearValues() {
   currentCalc.textContent = "0";
   previousCalc.textContent = "";
   operator = null;
-  one = null;
-  two = null;
+  operands = [];
   isWaiting = false;
   onlyOperator = false;
 }
@@ -136,25 +143,34 @@ function updatePreviousCalc() {
   if(onlyOperator){
     previousCalc.textContent += ` ${operator} `;
   } else if (operator == null) {
-    previousCalc.textContent += `${two}`;
+    previousCalc.textContent += `${(operands[currentOperand]).round(3)}`;
   } else {
-    previousCalc.textContent += `${two} ${operator} `;
+    previousCalc.textContent += `${(operands[currentOperand - 1]).round(3)} ${operator}`;
   }
 }
 
 //Logical functions
+const numFormat = new Intl.NumberFormat({ 
+  maximumFractionDigits: 3,
+ });
+
+ Number.prototype.round = function(n){
+   const decimals = Math.pow(10,n);
+   return Math.round((this + Number.EPSILON) * decimals) / decimals;
+ }
+
 function add() {
-  currentCalc.textContent = one + two;
+  currentCalc.textContent = `${(operands[currentOperand-1] + operands[currentOperand]).round(3)}`;
 }
 
 function subtract(numA, numB) {
-  currentCalc.textContent = one - two;
+  currentCalc.textContent = `${(operands[currentOperand-1] - operands[currentOperand]).round(3)}`;
 }
 
 function multiply(numA, numB) {
-  currentCalc.textContent = one * two;
+  currentCalc.textContent = `${(operands[currentOperand-1] * operands[currentOperand]).round(3)}`;
 }
 
 function divide(numA, numB) {
-  currentCalc.textContent = one / two;
+  currentCalc.textContent = `${(operands[currentOperand-1] / operands[currentOperand]).round(3)}`;
 }
